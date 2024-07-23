4ad5cd633a2d6a4b758154cc32379fdbc5f6af6d
JGardner01 <jamesjmg@live.co.uk>
2024-07-23 01:38:05 +0100

    Created OrganiseFiles function within the file organiser class, returning a boolean depending if it was successful. Changed default path in config class to user's downloads folder. Implemented onOrganiseButtonClick to create folders, organise files and then output the result.

--- src/main/java/com/jamesgardner/fileorganiser/FileOrganiser.java
+++ src/main/java/com/jamesgardner/fileorganiser/FileOrganiser.java
@@ -1,6 +1,11 @@
 package com.jamesgardner.fileorganiser;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.HashMap;
+import java.util.List;
 
 public class FileOrganiser {
     //config
@@ -23,6 +28,42 @@ public class FileOrganiser {
     }
 
     public boolean organiseFiles(){
-        return true;
+        try{
+            File directory = new File(path);
+            File[] files = directory.listFiles();
+
+            if (files == null){
+                System.out.println("No files found");
+                return true;
+            }
+
+            for (File file : files){
+                if (file.isFile()) {
+                    String fileName = file.getName().toLowerCase();
+
+                    for (HashMap.Entry<String, List<String>> fileTypes : Config.fileTypes.entrySet()) {
+                        String folder = fileTypes.getKey();
+                        List<String> extensions = fileTypes.getValue();
+                        for (String ext : extensions) {
+                            if (fileName.endsWith(ext)) {
+                                File destFolder = new File(path, folder);
+                                File currentFile = new File(destFolder, file.getName());
+                                try {
+                                    Files.move(file.toPath(), currentFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                                } catch (IOException e) {
+                                    System.err.println("Error moving file " + fileName + ": " + e.getMessage());
+                                }
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            return true;
+
+        } catch (Exception e){
+            System.err.println("Error organising files");
+            return false;
+        }
     }
 }
--- src/main/java/com/jamesgardner/fileorganiser/MainController.java
+++ src/main/java/com/jamesgardner/fileorganiser/MainController.java
@@ -5,10 +5,25 @@ import javafx.scene.control.Label;
 
 public class MainController {
     @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+    protected void onOrganiseButtonClick() {
+        System.out.println("Organising " + Config.defaultPath);
+
+
+        FileOrganiser fileOrganiser = new FileOrganiser();
+
+        fileOrganiser.createFolders();
+
+        boolean organised = fileOrganiser.organiseFiles();
+
+        if (organised){
+            System.out.print("Files were organised");
+        } else {
+            System.out.println("Error organising files");
+        }
+
     }
+
+
 }
