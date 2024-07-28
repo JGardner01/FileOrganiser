180587ea78420e72b4a9abfa83ff38a09f3181c6
JGardner01 <jamesjmg@live.co.uk>
2024-07-28 01:19:00 +0100

    Added additional app setting, change default directory.

--- src/main/java/com/jamesgardner/fileorganiser/AppSettingsController.java
+++ src/main/java/com/jamesgardner/fileorganiser/AppSettingsController.java
@@ -2,9 +2,16 @@ package com.jamesgardner.fileorganiser;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
+
 public class AppSettingsController {
+
+    @FXML
+    private TextField directoryTextField;
 
     @FXML
     private CheckBox backgroundCheckBox;
@@ -12,6 +19,19 @@ public class AppSettingsController {
     @FXML
     private CheckBox startUpCheckBox;
 
+    public void initialize() {
+        directoryTextField.setText(Config.defaultPath);
+    }
+
+    @FXML
+    protected void onSelectDirectoryButtonClick() {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Select Directory to Organise");
+
+        File selectedDirectory = directoryChooser.showDialog(new Stage());
+        System.out.println(selectedDirectory);//debug
+        directoryTextField.setText(String.valueOf(selectedDirectory));
+    }
 
     @FXML
     protected  void onCancelButtonClick(){
@@ -21,6 +41,17 @@ public class AppSettingsController {
 
     @FXML
     protected void onSaveButtonClick(){
+
+        String path = directoryTextField.getText();
+        File directory = new File(path);
+        //check directory is valid
+        if (!directory.exists() || !directory.isDirectory()){
+            System.out.println("Directory invalid");
+            return;
+        } else {
+            Config.defaultPath = path;
+        }
+
         AppSettings.setRunInBackground(backgroundCheckBox.isSelected());
         AppSettings.setRunAtStartUp(startUpCheckBox.isSelected());
         Stage stage = (Stage) backgroundCheckBox.getScene().getWindow();
